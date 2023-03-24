e931fa2cd09cfaabda2e228d3205fb74b6ade00d
lucasll98 <lucaasbj98@gmail.com>
2023-03-24 14:14:08 -0300

    Septimo commit: le agregue inicio de sesion capturados por js

--- paginaweb.js
+++ paginaweb.js
@@ -9,3 +9,15 @@ function enlaceIg(){
     location.href = "https://www.instagram.com/lucasl.98/";
 
 }
+
+function comentarios(){
+   var comentarios= document.getElementById("comentarios").value;
+    console.log("Comentarios: " , comentarios);
+}
+
+function iniciarSesion(){
+    var usuario = document.getElementById("usuario").value;
+    var contra = document.getElementById("contra").value;
+
+    console.log("Usuario: " , usuario + ". Contraseña: " , contra)
+}
